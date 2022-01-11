71757d74856bd6e4eeeacd6c493a4983631812be
Andreas Rozek <A.Rozek@gmx.de>
2022-01-11 07:40:42 +0100

    Create drawMoonPhase.js

---
+++ drawMoonPhase.js
@@ -0,0 +1,28 @@
+(function () {
+  if (g.drawMoonPhase == null) {
+    g.drawMoonPhase = function drawMoonPhase (
+      CenterX,CenterY, Radius, leftFactor,rightFactor
+    ) {
+      let x = Radius, y = 0, Error = Radius;
+
+      let gfx = this || g;
+
+      gfx.drawLine(CenterX-leftFactor*x,CenterY, CenterX+rightFactor*x,CenterY);
+
+      let dx,dy;
+      while (y <= x) {
+        dy = 1 + 2*y; y++; Error -= dy;
+        if (Error < 0) {
+          dx = 1 - 2*x; x--; Error -= dx;
+        }
+
+        gfx.drawLine(CenterX-leftFactor*x,CenterY-y, CenterX+rightFactor*x,CenterY-y);
+        gfx.drawLine(CenterX-leftFactor*x,CenterY+y, CenterX+rightFactor*x,CenterY+y);
+        gfx.drawLine(CenterX-leftFactor*y,CenterY-x, CenterX+rightFactor*y,CenterY-x);
+        gfx.drawLine(CenterX-leftFactor*y,CenterY+x, CenterX+rightFactor*y,CenterY+x);
+      }
+
+      return gfx;
+    };
+  }
+})();
